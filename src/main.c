61e94d99f27382fb55b5961eec0166036d9a7a6f
Stephan Richter <github@keawe.de>
2018-02-09 15:10:20 +0100

    Aufrufparameter

--- src/main.c
+++ src/main.c
@@ -1,7 +1,11 @@
 #include<stdio.h>
 #include "funktionen.h"
 
-int main() {
-	printTime();
+int main(int argc, char* argv[]) {
+	int i;
+	printf("argc: %d\n", argc);
+	for(i=0; i < argc; i++) {
+		printf("argv[%d]: %s\n", i, argv[i]);
+	}
 	return 0;
 }
