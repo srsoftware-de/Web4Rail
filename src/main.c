32fa06c79f5c4b1796ce4ac9444d47f9cbf42739
Stephan Richter <github@keawe.de>
2018-02-09 13:43:08 +0100

    Formatierungen

--- src/main.c
+++ src/main.c
@@ -1,7 +1,10 @@
 #include <stdio.h>
 int main(){
-	char c = '@';
-	printf( "Zeichen: %c\n", c);
+
+	int laenge=312500, breite=50;
+	printf("\nLaenge: %d cm\nBreite: %d cm\n", laenge, breite);
+	printf("\nLaenge: %10d cm\nBreite: %10d cm\n", laenge, breite);
+
 	getchar();
 	return 0;
 }
