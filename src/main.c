488b5382fbd3b3c3a03449df0a363a5a085ce55e
Stephan Richter <github@keawe.de>
2018-02-09 13:45:26 +0100

    einlesen einzelner Zeichen

--- src/main.c
+++ src/main.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
 int main(){
 
-	float laenge=312.5789, breite=5.6;
-	printf("\nLaenge: %f cm\nBreite: %f cm\n", laenge, breite);
-	printf("\nLaenge: %10.2f cm\nBreite: %10.2f cm\n", laenge, breite);
+	char c;
+	printf("Mit welchem Buchstaben beginnt ihr Vorname? ");
+	c = getchar();
+	printf("\nIch weiss jetzt, dass Ihr Vorname mit '%c' beginnt.\n", c);
 
-	getchar();
+
 	return 0;
 }
