9459357c29ae03622f356a2313edc2a8a0458ffc
Stephan Richter <github@keawe.de>
2018-02-09 14:27:02 +0100

    erste Strings

--- src/main.c
+++ src/main.c
@@ -1,11 +1,9 @@
 #include<stdio.h>
-
-// Symbolische Konstante
-#define MWSTFAKTOR 0.19
+#include<string.h>
 
 int main() {
-	float betrag = 350.0;
-	float mwst = betrag * MWSTFAKTOR;
-	printf("%.f% von %.2f Euro: %.2f Euro\n", MWSTFAKTOR * 100, betrag, mwst);
+	char string[] = "Die schoenen Dinge im Leben sind umsonst.";
+	int laenge = strlen(string);
+	printf("Laenge: %d Zeichen\n", laenge);
 	return 0;
 }
