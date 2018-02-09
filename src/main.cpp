c3e21b60228c0259d6a8581fbc9b93619f0082ee
Stephan Richter <github@keawe.de>
2018-02-09 16:29:53 +0100

    started new C++ project: first test

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,18 @@
-#include <iostream>
-
+#include <iostream>             // Schnittstellen fr die Streams einbinden
 using namespace std;
 
-int main(){
-   cout << "Hello World";
+int main(void) {
+	char c;                        // Variable fr ein gelesenes Zeichen
+	bool spc = false;                // Variable: War das letzte Zeichen ein Space?
 
-   return 0;
-}
+	while ( cin.get(c) ) {
+		if ( c == ' ' ){                 // Space?
+			if ( !spc ) cout << c;     // nur das jeweils erste Space ausgeben
+			spc = true;                // merken, da˜ Space gelesen
+		} else {
+			cout << c;               // Zeichen au˜er Spaces bernehmen
+			spc = false;               // merken, da˜ kein Space gelesen
+		}
+	}                             // Ende des Schleifen-Rumpfes
+	return 0;
+}                                // Ende des Hauptprogramms
