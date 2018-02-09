a277d41e8c890d41859079f8a280a96eb06a1f94
Stephan Richter <github@keawe.de>
2018-02-09 14:04:23 +0100

    Pointer

--- src/main.c
+++ src/main.c
@@ -1,29 +1,12 @@
 #include<stdio.h>
 
-// Funktions-Prototypen
-float eingabeZahl();
-float multipliziere(float zahl1, float zahl2);
-void ausgabeErgebnis(float ergebnis);
-
-// Hauptprogramm
 int main() {
-	// Rechenvorgang
-	ausgabeErgebnis(multipliziere(eingabeZahl(), eingabeZahl()));
+
+	int zahl = 7;
+	int *zeiger;
+	zeiger = &zahl;
+	printf("Zeiger-Adresse: %d\n", zeiger);
+	printf("Zeiger-Wert: %d\n", *zeiger);
+
 	return 0;
-}
-
-// Funktionen
-float eingabeZahl() {
-	float eingabe;
-	printf("\nEingabe Zahl: ");
-	scanf("%f", &eingabe);
-	return eingabe;
-}
-
-float multipliziere(float zahl1, float zahl2) {
-	return (zahl1 * zahl2);
-}
-
-void ausgabeErgebnis(float ergebnis) {
-	printf("\nErgebnis: %f\n", ergebnis);
 }
