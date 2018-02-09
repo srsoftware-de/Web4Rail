652ac0d872f21fa98dc3f7bbb4ce989c1fb31aeb
Stephan Richter <github@keawe.de>
2018-02-09 14:19:29 +0100

    Speicherallokation

--- src/main.c
+++ src/main.c
@@ -1,12 +1,24 @@
 #include<stdio.h>
+#include<stdlib.h>
 
 int main() {
+	int size=0;
+	int *array;
 
-	int zahl = 7;
-	int *zeiger;
-	zeiger = &zahl;
-	printf("Zeiger-Adresse: %d\n", zeiger);
-	printf("Zeiger-Wert: %d\n", *zeiger);
+	printf("Array-Groesse eingeben: ");
+	scanf("%d", &size);
+
+	// Speicher reservieren
+	array = (int *) calloc(size, sizeof(int));
+
+	if(array != NULL) {
+		printf("\nSpeicher ist reserviert\n");
+		free(array);
+		printf("\nSpeicher wurde wieder freigegeben\n");
+	}else {
+		printf("\nKein freier Speicher vorhanden.\n");
+	}
+
 
 	return 0;
 }
