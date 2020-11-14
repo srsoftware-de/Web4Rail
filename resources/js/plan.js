a9e7512e3d399562ba4c1775621f9b3a13a3d8ca
Stephan Richter <github@keawe.de>
2020-11-14 14:37:32 +0100

    re-mapped emergency to Enter key

--- resources/js/plan.js
+++ resources/js/plan.js
@@ -118,7 +118,7 @@ function heartbeat(data){
 }
 
 function keypress(ev){
-	if (ev.key === " ") request({realm:"cu",action:"emergency"})
+	if (ev.code === 'Enter') request({realm:"cu",action:"emergency"});
 }
 
 function moveTile(x,y){	
